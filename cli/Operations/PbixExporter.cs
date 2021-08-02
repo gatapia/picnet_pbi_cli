b222ab3f957d77910b447ea539183e8a57e03ad2
Guido Tapia <guido.tapia@picnet.com.au>
2021-08-03 07:12:06 +1000

    Export now working using DotNetZip library

--- cli/Operations/PbixExporter.cs
+++ cli/Operations/PbixExporter.cs
@@ -1,44 +1,49 @@
 ﻿using System;
 using System.IO;
-using System.IO.Compression;
+using System.Linq;
 using System.Text;
+using Ionic.Zip;
 
 namespace cli.Operations {
-  public class PbixExporter : IRun {
-    
-    
-    public void Run(CliOptions opts) { 
-      if (String.IsNullOrEmpty(opts.Output)) throw new Exception($"the pbiximporter requires the -o file_name.pbix parameter");
+  public class PbixExporter : IRun 
+  {
+
+    public void Run(CliOptions opts) 
+    { 
+      if (String.IsNullOrEmpty(opts.Output)) throw new Exception("the pbiximporter requires the -o file_name.pbix parameter");
+
       var dir = new DirectoryInfo(opts.File ?? Constants.DEFAULT_SRC_DIR);
       if (!dir.Exists) { throw new FileNotFoundException($"could not find the specified source directory \"{dir.Name}\""); }
       
       if (File.Exists(opts.Output)) File.Delete(opts.Output);
-      
-      using (var archive = ZipFile.Open(opts.Output, ZipArchiveMode.Create)) {
-        Array.ForEach(dir.GetFiles("*.*", SearchOption.AllDirectories), f => {
-          if (f.Name == Constants.GITIGNORE) return;
-          var name = f.FullName.Replace(dir.FullName + "\\", "");
-          if (!PbixHelpers.IsValidSourceFile(f)) {
-            Console.WriteLine($"adding binary entry [{name}]");
-            archive.CreateEntryFromFile(f.FullName, name);
-            return;
-          }
-          var contents = File.ReadAllText(f.FullName, Encoding.UTF8);
-          contents = PbixHelpers.FormatFileContentsImpl(f, contents, false);
-          var encoder = PbixHelpers.GetFileEncoding(f);
 
-          var entry = archive.CreateEntry(name);
-          using var stream = entry.Open();
-          using var writer = new StreamWriter(stream, encoder);
+      using var archive = new ZipFile(opts.Output);
+      dir.GetFiles("*.*", SearchOption.AllDirectories).
+          Where(f => f.Name != Constants.GITIGNORE).
+          ToList().
+          ForEach(f => {
+        var name = f.FullName.Replace(dir.FullName + "\\", "");
+        Console.WriteLine($"adding file to archive [{name}]");
 
-          // var encoded = encoder.GetBytes(contents);
-          Console.WriteLine($"adding text entry [{name}] encoding [{encoder.GetType().Name}]");
-          writer.Write(contents);
-        });
-      }
+        if (PbixHelpers.IsValidSourceFile(f)) { AddSourceFileToArchive(f, archive, name); }
+        else { AddBinaryFileToArchive(name, archive, f, dir); }
+      });
+      archive.Save();
       Console.WriteLine($"pbix file [{opts.Output}] created");
-      Directory.Delete(opts.Output + "dir", true);
-      ZipFile.ExtractToDirectory(opts.Output, opts.Output + "dir");
+    }
+
+    private static void AddBinaryFileToArchive(string name, ZipFile archive, FileInfo f, DirectoryInfo dir)
+    {
+      archive.AddFile(f.FullName, f.Directory?.FullName.Replace(dir.FullName, ""));
+    }
+
+    private static void AddSourceFileToArchive(FileInfo f, ZipFile archive, string name)
+    {
+      var contents = File.ReadAllText(f.FullName, Encoding.UTF8);
+      contents = PbixHelpers.FormatFileContentsImpl(f, contents, false);
+      var encoder = PbixHelpers.GetFileEncoding(f);
+      var bytes = encoder.GetBytes(contents);
+      archive.AddEntry(name, bytes);
     }
   }
 }
